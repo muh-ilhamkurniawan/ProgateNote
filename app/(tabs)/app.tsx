382a15ad09f9835ad15387272c1731e6f5d38011
muh-ilhamkurniawan <93339557+muh-ilhamkurniawan@users.noreply.github.com>
2024-06-14 13:16:25 +0700

    Add Edit and Delete Note

--- app/(tabs)/app.tsx
+++ app/(tabs)/app.tsx
@@ -1,53 +1,40 @@
-import React, { useState } from 'react'
-import Home from '@/src/screens/home'
-import AddNote from '@/src/screens/addNote'
-import EditNote from '@/src/screens/editNote'
+import React, { useState } from 'react';
+import Home from '@/src/screens/home';
+import AddNote from '@/src/screens/addNote';
+import EditNote from '@/src/screens/editNote';
 
-// Tambahkan function "addNote" sebagai prop
 const CurrentPageWidget = ({
   currentPage,
   noteList,
   setCurrentPage,
   addNote,
+  editNote,
+  deleteNote,
+  selectedNote,
+  setSelectedNote
 }) => {
   switch (currentPage) {
     case 'home':
-      return <Home noteList={noteList} setCurrentPage={setCurrentPage} />
+      return <Home noteList={noteList} setCurrentPage={setCurrentPage} deleteNote={deleteNote} setSelectedNote={setSelectedNote} />;
     case 'add':
-      // Berikan function "addNote" ke component "AddNote"
-      return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />
+      return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />;
     case 'edit':
-      return <EditNote />
+      return <EditNote note={selectedNote} setCurrentPage={setCurrentPage} editNote={editNote} />;
     default:
-      return <Home />
+      return <Home />;
   }
-}
-
-const addNote = (title, desc) => {
-  const id =
-    noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1
-
-  setNoteList([
-    ...noteList,
-    {
-      id,
-      title: title,
-      desc: desc,
-    },
-  ])
-}
+};
 
 const App = () => {
-  const [currentPage, setCurrentPage] = useState('home')
-
+  const [currentPage, setCurrentPage] = useState('home');
   const [noteList, setNoteList] = useState([
     {
       id: 1,
       title: 'Note pertama',
-      desc:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
     },
-  ])
+  ]);
+  const [selectedNote, setSelectedNote] = useState(null);
 
   const addNote = (title, desc) => {
     const id = noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1;
@@ -55,10 +42,22 @@ const App = () => {
       ...noteList,
       {
         id,
-        title: title,
-        desc: desc,
+        title,
+        desc,
       },
     ]);
+  };
+
+  const editNote = (id, title, desc) => {
+    const updatedNotes = noteList.map(note => 
+      note.id === id ? { ...note, title, desc } : note
+    );
+    setNoteList(updatedNotes);
+  };
+
+  const deleteNote = (id) => {
+    const updatedNotes = noteList.filter(note => note.id !== id);
+    setNoteList(updatedNotes);
   };
 
   return (
@@ -66,10 +65,13 @@ const App = () => {
       currentPage={currentPage}
       noteList={noteList}
       setCurrentPage={setCurrentPage}
-      // Berikan function addNote sebagai prop
       addNote={addNote}
+      editNote={editNote}
+      deleteNote={deleteNote}
+      selectedNote={selectedNote}
+      setSelectedNote={setSelectedNote}
     />
   );
-}
+};
 
-export default App
+export default App;
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,61 +1,54 @@
-import React from 'react'
-import { FlatList, StyleSheet, View, Text } from 'react-native'
-import CustomButton from '../components/customButton'
+import React from 'react';
+import { FlatList, StyleSheet, View, Text } from 'react-native';
+import CustomButton from '../components/customButton';
 
-
-// Tambahkan "setCurrentPage" sebagai sebuah prop
-const NoteCard = ({ item, setCurrentPage }) => (
-    <View style={styles.card}>
-      <Text style={styles.cardTitle}>{item.title}</Text>
-      <Text>{item.desc}</Text>
-      <View style={styles.buttons}>
-        <CustomButton
-          backgroundColor="#FFC300"
-          color="#151D3B"
-          text="Ubah"
-          fontSize={12}
-          width={100}
-          // Tuliskan layar "edit" untuk ketika tombol-nya ditekan
-          onPress={() => {
-            setCurrentPage('edit')
-          }}
-        />
-        <CustomButton
-          backgroundColor="#D82148"
-          color="#fff"
-          text="Hapus"
-          fontSize={12}
-          width={100}
-          onPress={() => {}}
-        />
-      </View>
-    </View>
-  )
-  
-  // Tambahkan "setCurrentPage" sebagai sebuah prop
-  const Home = ({ noteList, setCurrentPage }) => (
-    <View style={styles.container}>
+const NoteCard = ({ item, setCurrentPage, deleteNote, setSelectedNote }) => (
+  <View style={styles.card}>
+    <Text style={styles.cardTitle}>{item.title}</Text>
+    <Text>{item.desc}</Text>
+    <View style={styles.buttons}>
       <CustomButton
-        backgroundColor="#DDD"
-        color="#203239"
-        text="Tambahkan Note"
-        width="100%"
-        // Tuliskan layar "add" untuk ketika tombol-nya ditekan
+        backgroundColor="#FFC300"
+        color="#151D3B"
+        text="Ubah"
+        fontSize={12}
+        width={100}
         onPress={() => {
-          setCurrentPage('add')
+          setSelectedNote(item);
+          setCurrentPage('edit');
         }}
       />
-      <FlatList
-        showsVerticalScrollIndicator={false}
-        data={noteList}
-        // Berikan function "setCurrentPage" ke component "NoteCard"
-        renderItem={({ item }) => (
-          <NoteCard item={item} setCurrentPage={setCurrentPage} />
-        )}
-        keyExtractor={(item) => item.id}
+      <CustomButton
+        backgroundColor="#D82148"
+        color="#fff"
+        text="Hapus"
+        fontSize={12}
+        width={100}
+        onPress={() => deleteNote(item.id)}
       />
     </View>
-  )
+  </View>
+);
+
+const Home = ({ noteList, setCurrentPage, deleteNote, setSelectedNote }) => (
+  <View style={styles.container}>
+    <CustomButton
+      backgroundColor="#DDD"
+      color="#203239"
+      text="Tambahkan Note"
+      width="100%"
+      onPress={() => setCurrentPage('add')}
+    />
+    <FlatList
+      showsVerticalScrollIndicator={false}
+      data={noteList}
+      renderItem={({ item }) => (
+        <NoteCard item={item} setCurrentPage={setCurrentPage} deleteNote={deleteNote} setSelectedNote={setSelectedNote} />
+      )}
+      keyExtractor={(item) => item.id.toString()}
+    />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -83,6 +76,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-evenly',
   },
-})
+});
 
-export default Home
+export default Home;
